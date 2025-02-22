02dffa775ed03d2deb9af11f6e8d12c51e01d9da
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-22 15:33:30 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 30)
+    basic.pause(1000)
+    maqueen.motorStop(maqueen.Motors.All)
+})
