641ea18548b6602302df705f160fe6c10a660820
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-25 14:27:12 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 30)
-    basic.pause(1000)
+    basic.pause(700)
     maqueen.motorStop(maqueen.Motors.All)
 })
